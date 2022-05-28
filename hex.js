d45d08152f53109c1c6bec90f42884bead681296
douglas mogoba <mogobanyamwaro@gmail.com>
2022-05-28 16:05:08 +0300

    color flipper finished

--- hex.js
+++ hex.js
@@ -1,1 +1,19 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const btn = document.getElementById('btn');
+
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  let hexColor = '#';
+  hex.forEach((_, index) => {
+    if (index < 6) {
+      hexColor += hex[getRandomNumber()];
+    }
+  });
+  document.body.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
+});
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * hex.length);
+};
